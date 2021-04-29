4b7d96654c508f1426ba547131a9c7d14cadb446
Oliver Fawcett-Griffiths <olly@ollyfg.com>
2021-04-29 12:11:52 +1200

    bugfix

--- packages/react-scripts/config/paths.js
+++ packages/react-scripts/config/paths.js
@@ -35,7 +35,7 @@ const publicUrlOrPath = getPublicUrlOrPath(
   process.env.PUBLIC_URL
 );
 
-const buildPath = `${pathToApp}${process.env.BUILD_PATH || 'build'}`;
+const buildPath = process.env.BUILD_PATH || 'build';
 
 const moduleFileExtensions = [
   'web.mjs',
